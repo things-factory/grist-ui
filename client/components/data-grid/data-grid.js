324b0e2cc1f21eb1f1d0fea853ddb1280b68a33b
Hearty Oh <heartyoh@hatiolab.com>
2019-07-24 00:38:40 +0900

    v0.2.22

--- client/components/data-grid/data-grid.js
+++ client/components/data-grid/data-grid.js
@@ -169,7 +169,7 @@ class DataGrid extends LitElement {
        * 데이타 내용에 따라 동적으로 컬럼의 폭이 달라지는 경우(예를 들면, sequence field)가 있으므로,
        * data의 변동에 대해서도 다시 계산되어야 한다.
        */
-      this.calculateWidths(this.config.columns)
+      this.calculateWidths(this.config && this.config.columns)
     }
   }
 
