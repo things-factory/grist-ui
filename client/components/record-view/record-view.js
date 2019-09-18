3d6edde6614ccfa04ba95a57ae5ccc4eb25044cb
hoppaang <hoppaang@hatiolab.com>
2019-09-18 18:17:54 +0800

    record view style

--- client/components/record-view/record-view.js
+++ client/components/record-view/record-view.js
@@ -30,26 +30,32 @@ export class RecordView extends LitElement {
         font: var(--record-view-header-font);
       }
       [footer] {
+        display: flex;
         text-align: right;
         background-color: var(--record-view-footer-background);
         box-shadow: var(--context-toolbar-shadow-line);
         height: var(--record-view-footer-height);
       }
       [footer] button {
+        flex: 1;
         background-color: transparent;
-        border: none;
+        border: 1px solid rgba(0, 0, 0, 0.3);
+        border-width: 0 0 0 1px;
         height: var(--record-view-footer-button-height);
-        padding: var(--record-view-footer-button-padding);
         color: var(--record-view-footer-button-color);
-        text-transform: capitalize;
+        font-size: 17px;
+        line-height: 3;
+      }
+      [footer] button * {
+        vertical-align: middle;
       }
       [footer] button mwc-icon {
-        vertical-align: middle;
-        margin: auto;
-        width: var(--record-view-footer-iconbutton-size);
-        margin-bottom: var(--record-view-footer-iconbutton-margin);
-        display: var(--record-view-footer-iconbutton-display);
+        margin-top: -3px;
+        margin-right: 5px;
         font-size: var(--record-view-footer-iconbutton-size);
+      }
+      [footer] button[ok] {
+        background-color: var(--record-view-footer-focus-background);
       }
     `
   }
@@ -70,8 +76,9 @@ export class RecordView extends LitElement {
       </div>
       <record-view-body .columns=${this.columns} .record=${this.record} .rowIndex=${this.rowIndex}> </record-view-body>
       <div footer>
-        <button><mwc-icon>clear</mwc-icon>reset</button>
-        <button><mwc-icon>done_all</mwc-icon>submit</button>
+        <button><mwc-icon>refresh</mwc-icon>Reset</button>
+        <button><mwc-icon>clear</mwc-icon>Cancel</button>
+        <button ok><mwc-icon>radio_button_unchecked</mwc-icon>OK</button>
       </div>
     `
   }
