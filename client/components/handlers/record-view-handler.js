ecbead82b72ceb52228339bb1793fd451d1381a1
Hearty Oh <heartyoh@hatiolab.com>
2019-09-20 17:34:06 +0800

    v0.3.10

--- client/components/handlers/record-view-handler.js
+++ client/components/handlers/record-view-handler.js
@@ -2,8 +2,6 @@ import { html } from 'lit-html'
 import { openPopup } from '@things-factory/layout-base'
 import '../record-view/record-view'
 
-const STYLE = 'width: 50vw;height: 50vh'
-
 /*
  * handler들은 data-grid-field 로부터 호출되는 것을 전제로 하며,
  * 전반적인 처리를 위해서, columns 및 data 정보를 포함해서 제공할 수 있어야 한다.
@@ -13,7 +11,6 @@ export const RecordViewHandler = function(columns, data, column, record, rowInde
   openPopup(
     html`
       <record-view
-        style=${STYLE}
         .field=${field}
         .columns=${columns}
         .column=${column}
@@ -22,7 +19,8 @@ export const RecordViewHandler = function(columns, data, column, record, rowInde
       ></record-view>
     `,
     {
-      backdrop: true
+      backdrop: true,
+      size: 'large'
     }
   )
 }
