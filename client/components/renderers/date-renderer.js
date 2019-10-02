7afa5a53e6dfe3bf40850f581b46f84fc707cf01
leemyunggyu <jaylee@hatiolab.com>
2019-10-02 14:11:03 +0800

    v0.8.3

--- client/components/renderers/date-renderer.js
+++ client/components/renderers/date-renderer.js
@@ -1,9 +1,19 @@
 // TODO 로케일 설정에 따라서 포맷이 바뀌도록 해야한다.
-const OPTIONS = {
+const DATETIME_OPTIONS = {
+  year: 'numeric',
+  month: 'numeric',
+  day: 'numeric',
+  hour: 'numeric',
+  minute: 'numeric',
+  second: 'numeric',
+  hour12: false
+  // timeZone: 'America/Los_Angeles'
+}
+
+const DATE_OPTIONS = {
   year: 'numeric',
   month: 'numeric',
   day: 'numeric'
-  // timeZone: 'America/Los_Angeles'
 }
 
 export const DateRenderer = (value, column, record, rowIndex, field) => {
@@ -11,7 +21,9 @@ export const DateRenderer = (value, column, record, rowIndex, field) => {
     return ''
   }
 
-  var options = column.record.options || OPTIONS
+  if (!isNaN(Number(value))) value = Number(value)
+
+  var options = column.record.options || column.type === 'datetime' ? DATETIME_OPTIONS : DATE_OPTIONS
   var formatter = new Intl.DateTimeFormat(navigator.language, options)
 
   return formatter.format(new Date(value))
