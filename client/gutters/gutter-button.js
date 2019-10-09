529be38079d99e9374f6c88627d40fcd86544691
Hearty Oh <heartyoh@hatiolab.com>
2019-10-10 02:36:27 +0800

    Update gutter-button.js

--- client/gutters/gutter-button.js
+++ client/gutters/gutter-button.js
@@ -5,9 +5,7 @@ export class GutterButton {
   static instance(config = {}) {
     var { icon = 'edit' } = config
 
-    if (typeof icon !== 'function') {
-      icon = () => icon
-    }
+    var iconFn = typeof icon == 'function' ? icon : () => icon
 
     var inlineHeaderStyle = 'font-size: var(--grid-header-fontsize, 13px);vertical-align: middle;'
     var inlineRecordStyle = 'font-size: var(--grid-record-fontsize, 13px);vertical-align: middle;'
@@ -23,14 +21,14 @@ export class GutterButton {
         header: {
           renderer: function(column) {
             return html`
-              <mwc-icon style=${inlineHeaderStyle}>${icon()}</mwc-icon>
+              <mwc-icon style=${inlineHeaderStyle}>${iconFn()}</mwc-icon>
             `
           }
         },
         record: {
           renderer: function(value, column, record, rowIndex, field) {
             return html`
-              <mwc-icon style=${inlineRecordStyle} center>${icon(record)}</mwc-icon>
+              <mwc-icon style=${inlineRecordStyle} center>${iconFn(record)}</mwc-icon>
             `
           },
           align: 'center'
