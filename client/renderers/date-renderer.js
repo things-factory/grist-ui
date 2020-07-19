06784c096648e5d425857bdeff436fb24a45b812
Hearty Oh <heartyoh@hatiolab.com>
2020-07-20 02:09:46 +0900

    #12

--- client/renderers/date-renderer.js
+++ client/renderers/date-renderer.js
@@ -7,7 +7,6 @@ const DATETIME_OPTIONS = {
   minute: 'numeric',
   second: 'numeric',
   hour12: false
-  // timeZone: 'America/Los_Angeles'
 }
 
 const DATE_OPTIONS = {
@@ -16,6 +15,27 @@ const DATE_OPTIONS = {
   day: 'numeric'
 }
 
+const TIME_OPTIONS = {
+  hour: 'numeric',
+  minute: 'numeric',
+  second: 'numeric'
+}
+
+const OPTIONS = {
+  datetime: DATETIME_OPTIONS,
+  time: TIME_OPTIONS,
+  date: DATE_OPTIONS
+}
+
+function getsec(time) {
+  var arr = time.split(':')
+  return arr[0] * 3600 + arr[1] * 60 + (arr[2] || 0)
+}
+
+/* 
+  TODO date, time, month, week 등 datetime 이 외의 타입인 경우에는 문자열로 처리하도록 한다. 
+  따라서, 이 경우는 타임존에 대한 처리가 불필요하다.
+*/
 export const DateRenderer = (value, column, record, rowIndex, field) => {
   if (!value) {
     return ''
@@ -23,8 +43,15 @@ export const DateRenderer = (value, column, record, rowIndex, field) => {
 
   if (!isNaN(Number(value))) value = Number(value)
 
-  var options = column.record.options || column.type === 'datetime' ? DATETIME_OPTIONS : DATE_OPTIONS
+  var options = column.record.options || OPTIONS[column.type]
   var formatter = new Intl.DateTimeFormat(navigator.language, options)
 
-  return formatter.format(new Date(value))
+  switch (column.type) {
+    case 'datetime':
+      return formatter.format(new Date(value))
+    case 'date':
+      return formatter.format(new Date(value))
+    case 'time':
+      return formatter.format(new Date(typeof value == 'string' ? getsec(value) * 1000 : value))
+  }
 }
